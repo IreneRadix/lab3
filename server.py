f681135bbb486f410e89355296f7f8eaa0bd92bb
IreneRadix <112644982+IreneRadix@users.noreply.github.com>
2024-04-04 15:25:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,47 +1,13 @@
-import os
 import socket
 import json
-
-
-class Program:
-    def __init__(self):
-        self.directory = os.getcwd()
-
-    def update_directory(self, _directory):
-        self.directory = os.path.dirname(_directory)
-
-    def get_directory_data(self):
-        file_info = []
-        for root, dirs, files in os.walk(self.directory):
-            for file in files:
-                file_path = os.path.join(root, file)
-                stat_info = os.stat(file_path)
-                file_info.append({
-                    "name": file,
-                    "path": file_path,
-                    "size": stat_info.st_size,
-                    "last_modified": stat_info.st_mtime
-                })
-        return file_info
-
-    @staticmethod
-    def save_file_info(file_info):
-        with open("files_info.json", "w") as json_file:
-            json.dump(file_info, json_file, indent=4)
-
-    @staticmethod
-    def get_binary_file_info():
-        with open("files_info.json", "rb") as json_file:
-            data = json_file.read()
-            return data
-
+from program import Program
+from threading import Thread
 
 prog = Program()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('localhost', 65432))
 s.listen(3)
-
 
 def make_file():
   path_dirs = os.getenv('PATH').split(os.pathsep)
@@ -73,14 +39,14 @@ def handle_commands(client):
         prog.save_file_info(prog.get_directory_data())
         file_data = prog.get_binary_file_info()
         client.sendall(file_data)
+    client_socket.close()
 
-    if data.decode() == 'var_4':
-      client.sendall(make_file())
+    if data.decode() == 'GET_JSON_FILE':
+        client.sendall(make_file())
 
 
 
 while True:
     client_socket, address = s.accept()
     print("Connected by", address)
-    handle_commands(client_socket)
-    client_socket.close()
+    Thread(target=handle_commands, args=(client_socket,)).start()
