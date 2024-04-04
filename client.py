a2af7809488fe1a2a2e64b78709ddc123c25a0e9
IreneRadix <112644982+IreneRadix@users.noreply.github.com>
2024-04-04 11:56:40 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+#from signal import signal, SIGPIPE, SIG_DFL
+#signal(SIGPIPE,SIG_DFL)
+
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('localhost', 9090))
+
+msg = input()
+sock.send(msg.encode())
+
+#file = open('./received_file.txt', 'w')
+#data = sock.recv(1024)
+while True:
+    data = sock.recv(1024)
+
+    if not data:
+        break
+    print(data.decode())
+    #file.write(data.decode())
+
+sock.close()
+
+print(data.decode())
