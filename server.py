7e6b54bd283ccede3e469f9146fe47c04e6e7878
IreneRadix <112644982+IreneRadix@users.noreply.github.com>
2024-04-04 13:45:12 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,114 +1,86 @@
-import json
-import socket
-import sys
-import time
-import threading
 import os
+import socket
+import json
 
-def run_server(port=9090):
-  serv_sock = create_serv_sock(port)
-  cid = 0
-  while True:
-    client_sock = accept_client_conn(serv_sock, cid)
-    t = threading.Thread(target=serve_client,
-                         args=(serv_sock, client_sock, cid))
-    t.start()
-    cid += 1
 
-def serve_client(serv_sock, client_sock, cid):
-  while True:
-    request = read_request(client_sock)
-    if request is None:
-      print(f'Client #{cid} unexpectedly disconnected')
-      break
-    else:
-      if 'exit' in request.decode('utf-8'):
-        write_response_close(client_sock, cid)
-        break
-      if 'sstop' in request.decode('utf-8'):
-        write_response_closes(serv_sock, client_sock, cid)
-        break
-      response = handle_request(request)
-      write_response(client_sock, response)
+class Program:
+    def __init__(self):
+        self.directory = os.getcwd()
 
-def create_serv_sock(serv_port):
-  serv_sock = socket.socket(socket.AF_INET,
-                            socket.SOCK_STREAM,
-                            proto=0)
-  serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def update_directory(self, _directory):
+        self.directory = os.path.dirname(_directory)
 
-  serv_sock.bind(('', serv_port))
-  serv_sock.listen()
-  return serv_sock
+    def get_directory_data(self):
+        file_info = []
+        for root, dirs, files in os.walk(self.directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                stat_info = os.stat(file_path)
+                file_info.append({
+                    "name": file,
+                    "path": file_path,
+                    "size": stat_info.st_size,
+                    "last_modified": stat_info.st_mtime
+                })
+        return file_info
 
-def accept_client_conn(serv_sock, cid):
-  client_sock, client_addr = serv_sock.accept()
-  print(f'Client #{cid} connected '
-        f'{client_addr[0]}:{client_addr[1]}')
-  return client_sock
+    @staticmethod
+    def save_file_info(file_info):
+        with open("files_info.json", "w") as json_file:
+            json.dump(file_info, json_file, indent=4)
 
-def read_request(client_sock):
-  request = bytearray()
-  try:
-    request = client_sock.recv(1024)
-    if not request:
-      # Клиент преждевременно отключился.
-      return None
-    return request
+    @staticmethod
+    def get_binary_file_info():
+        with open("files_info.json", "rb") as json_file:
+            data = json_file.read()
+            return data
 
-  except ConnectionResetError:
-    # Соединение было неожиданно разорвано.
-    return None
-  except:
-    raise
+
+prog = Program()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 65432))
+s.listen(3)
+
 
 def make_file():
-    path_dirs = os.getenv('PATH').split(os.pathsep)
-    data = {}
+  path_dirs = os.getenv('PATH').split(os.pathsep)
+  data = {}
 
-    for path_dir in path_dirs:
-      for root, dirs, files in os.walk(path_dir):
-        executables = [f for f in files if os.access(os.path.join(root, f), os.X_OK)]
-        if executables:
-          data[root] = executables
+  for path_dir in path_dirs:
+    for root, dirs, files in os.walk(path_dir):
+      executables = [f for f in files if os.access(os.path.join(root, f), os.X_OK)]
+      if executables:
+        data[root] = executables
 
-    with open('./program_data.json', 'w') as file:
-      json.dump(data, file, indent=4)
+  with open('./program_data.json', 'w') as file:
+    json.dump(data, file, indent=4)
 
-    f = open('./program_data.json')
-    resp = f.read()
-    f.close()
+  f = open('./program_data.json')
+  resp = f.read()
+  f.close()
+
+  return resp.encode()
+
+def handle_commands(client):
+    data = client.recv(1024)
+    if data.decode().startswith('CH_DIR'):
+        new_dir = data.decode().split('CH_DIR', 1)[1].strip()
+        prog.update_directory(new_dir)
+        client.sendall(b'ok your changes accepted')
+
+    if data.decode() == 'GET_FILE':
+        prog.save_file_info(prog.get_directory_data())
+        file_data = prog.get_binary_file_info()
+        client.sendall(file_data)
+
+    if data.decode() == 'var_4':
+      client.sendall(make_file())
 
 
-    return resp
 
-
-def handle_request(request):
-  #time.sleep(5)
-  if request.decode('utf-8') == 'var_4':
-    return make_file().encode('utf-8')
-  # здесь дописываешь if со своим вариантом, и вызываешь функцию, которая возвращает то, что сервер должен выдавать в соответствии с вариантом
-
-def write_response(client_sock, response):
-  '''
-   while True:
-      file_data = file.read(1024)
-      conn.send(file_data)
-      if not file_data:
-        break
-    conn.close()
-  '''
-  client_sock.sendall(response)
-
-def write_response_close(client_sock, cid):
-  client_sock.close()
-  print(f'Client #{cid} has been served')
-
-def write_response_closes(serv_sock, client_sock, cid):
-  client_sock.close()
-  serv_sock.close()
-  print(f'Client #{cid} has been stoped server')
-  os._exit(0)
-
-if __name__ == '__main__':
-  run_server(port=9090)
+while True:
+    client_socket, address = s.accept()
+    print("Connected by", address)
+    handle_commands(client_socket)
+    client_socket.close()
