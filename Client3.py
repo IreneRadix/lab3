7ac07c10238628be99b9314bb780d5ac5b55c445
Anastar12 <113055619+Anastar12@users.noreply.github.com>
2024-04-04 16:38:34 +0300

    Add files via upload
    First commit: Client3.py

---
+++ Client3.py
@@ -0,0 +1,16 @@
+import socket
+
+def get_program_data():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8000))
+    data = b''
+    while True:
+        recv_data = client_socket.recv(1024)
+        if not recv_data:
+            break
+        data += recv_data
+    client_socket.close()
+    return data
+
+program_data = get_program_data()
+print(program_data.decode('utf-8'))
